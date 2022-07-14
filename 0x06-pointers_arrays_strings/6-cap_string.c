e66f0c1be3564c084395af75fcc16bce09c0ba85
tmnyaega <mogendinyaega@gmail.com>
2022-07-14 10:47:18 -0700

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,7 @@
 
 /**
  *  * cap_string - capitalizes all words of a string
- *
  *   * @s: input string.
- *
  *    * Return: the pointer to dest.
  */
 
@@ -15,23 +13,23 @@ char *cap_string(char *s)
 
 	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
-		count++;
-
-		while (*(s + count) != '\0')
+	count++;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
 		{
-			for (i = 0; i < 13; i++)
+			if (*(s + count) == separators[i])
 			{
-				if (*(s + count) == separators[i])
-				{
-					if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-						*(s + (count + 1)) = *(s + (count + 1)) - 32;
-					break;
-				}
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;			
 			}
-			count++;
 		}
-		return (s);
+		count++;
+	}
+
+	return (s);
+
 }
