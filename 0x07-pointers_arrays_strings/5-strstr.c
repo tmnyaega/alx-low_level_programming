ece66a08c906dfedb1fded104964cd3d7cbb0ab5
Tiberius <82136574+tmnyaega@users.noreply.github.com>
2022-07-18 12:53:27 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,6 @@
  * _strstr - locates a substring
  * @haystack: string to be searched in
  * @needle: string to be searched
- *
  * Return: Returns a pointer to the beginning of the located substring
  */
 
