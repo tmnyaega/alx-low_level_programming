9659f72d0f8626f38f1df0a713daeae999d15c44
Tiberius <82136574+tmnyaega@users.noreply.github.com>
2022-07-18 13:33:34 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c); 
 void print_diagsums(int *, int);
 void set_string(char **s, char *to);
 
